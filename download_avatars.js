5eca00b05035489884f2a3f0e2724dfaa1cb6bd9
GaganLal <lalgagan@outlook.com>
2019-01-15 02:54:55 +0000

    step 8

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,23 @@
 var request = require('request')
 var token = require('./secrets.js')
+var fs = require('fs')
+
+function downloadImageByURL(url, filePath) {
+  // console.log(url, filePath);
+  fs.existsSync("avatars") || fs.mkdirSync("avatars");
+  request.get(url).pipe(fs.createWriteStream(filePath));
+}
 
 var parsingFunction = function(responseBody) {
   var parsed = JSON.parse(responseBody)
-  console.log(parsed)
+  //console.log(parsed)
+parsed.forEach(function(user) {
+  const url = user.avatar_url;
+  const filePath = `avatars/${user.login}.jpg`
+  downloadImageByURL(url, filePath)
+})
+
+
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -19,6 +33,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   });
 }
+
+
+
+
 
 getRepoContributors("jquery", "jquery", parsingFunction)
 
