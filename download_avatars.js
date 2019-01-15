5d87b9707418ac1df86fe67e1575e3f92c623356
GaganLal <lalgagan@outlook.com>
2019-01-15 01:57:24 +0000

    first step

--- download_avatars.js
+++ download_avatars.js
@@ -1,18 +1,25 @@
 var request = require('request')
 var token = require('./secrets.js')
 
+var parsingFunction = function(responseBody) {
+  var parsed = JSON.parse(responseBody)
+  console.log(parsed)
+}
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': token.GITHUB_TOKEN
+      'Authorization': "token " + token.GITHUB_TOKEN
     }
   };
-
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(body);
+
   });
 }
 
-getRepoContributors("jquery", "jquery", request)
+getRepoContributors("jquery", "jquery", parsingFunction)
+
+
